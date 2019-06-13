5d536533b98ac965fa27cbfe6c1ffd0faf6b2be3
gokuexe <48706633+gokuexe@users.noreply.github.com>
2019-06-13 19:45:18 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule }                     from '@angular/core';
+import { RouterModule, Routes }         from '@angular/router';
+import {Page404Component} from './page-404.component';
+
+const enlacesRoutes: Routes = [
+  { 
+    path: '', 
+    loadChildren: './enlaces/enlaces.module#EnlacesModule'
+  },
+];
+
+const routes: Routes = [
+  {
+    path: 'enlaces',
+    children: enlacesRoutes
+  },
+  {
+    path: '**',
+    component: Page404Component,
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes, 
+                {useHash: false , enableTracing: false}) 
+            ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
